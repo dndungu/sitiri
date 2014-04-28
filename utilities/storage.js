1466bc9cd809dce72eb2fc9cf354eb0f6a1b9579
David Njuguna <dnjuguna@gmail.com>
2014-04-28 12:28:19 +0300

    Added setter and getter to storage

--- utilities/storage.js
+++ utilities/storage.js
@@ -1,6 +1,15 @@
 var mongodb = require('mongodb');
 
+var _private = {};
+
 module.exports = {
+        set : function(){
+                _private[arguments[0]] = arguments[1];
+        },
+        get : function(){
+                var key = arguments[0];
+                return _private[key] ? _private[key] : null;
+        },	
         db : function(settings){
                 var server = new mongodb.Server(settings.database.host, settings.database.port, settings.database.server_options);
                 return new mongodb.Db(settings.database.name, server, settings.database.db_options);
