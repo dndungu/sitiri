3fd33cd1134047836d85f905c617fd783a277f8a
David Njuguna <dnjuguna@gmail.com>
2014-04-29 11:43:37 +0300

    Changed the setter and getter dictionary store to _private.store

--- utilities/storage.js
+++ utilities/storage.js
@@ -1,14 +1,16 @@
 var mongodb = require('mongodb');
 
-var _private = {};
+var _private = {
+	store : {}
+};
 
 module.exports = {
         set : function(){
-                _private[arguments[0]] = arguments[1];
+                _private.store[arguments[0]] = arguments[1];
         },
         get : function(){
                 var key = arguments[0];
-                return _private[key] ? _private[key] : null;
+                return _private.store[key] ? _private.store[key] : null;
         },	
         db : function(settings){
                 var server = new mongodb.Server(settings.database.host, settings.database.port, settings.database.server_options);
