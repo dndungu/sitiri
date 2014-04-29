d9a30d04b33cb4f52ba4867df8cba4da049b1960
David Njuguna <dnjuguna@gmail.com>
2014-04-29 20:02:49 +0300

    Added core controller

---
+++ core/controller.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var router = require("../core/router.js");
+var authenticator = require("../core/authenticator.js");
+var operator = require("../core/operator.js");
+var publisher = require("../core/publisher.js");
+var storage = require('../utilities/storage.js');
+var broker = require('../utilities/broker.js');
+var cookies = require('../utilities/cookies.js');
+var context = require('../utilities/context.js');
+
+var _private = {
+	createServer: function(settings){
+		http.createServer(function(request, response){
+			var host = request.headers.host.split(':')[0];
+			var uri = String(request.url.trim()).toLowerCase();
+			var requestContext = new context();
+			var contextBroker = new broker();
+			var contextCookies = new cookies(request);
+			requestContext.set("cookies", contextCookies);
+			requestContext.set("request", request);
+			requestContext.set("response", response);
+			requestContext.set("host", host);
+			requestContext.set("method", request.method);
+			requestContext.set("uri", uri);
+			requestContext.set("storage", storage);
+			requestContext.set("settings", settings);
+			requestContext.set("broker", contextBroker);
+			router.init(requestContext);
+			authenticator.init(requestContext);
+			operator.init(requestContext);
+			publisher.init(requestContext);
+			requestContext.get("broker").emit({type : 'controller.passed', data : requestContext});
+		}).listen(settings.server.port, settings.server.host);		
+	}
+};
+
+module.exports = {
+	listen: function(settings){
+		storage.db(settings).open(function(error, db){
+			if(error){
+				console.log(error);
+				return
+			}
+			storage.set("global", db);
+			_private.createServer(settings);
+		});
+	}
+};
