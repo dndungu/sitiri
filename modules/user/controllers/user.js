832321832675445ef44307a1e11860fc2735600f
David Njuguna <dnjuguna@gmail.com>
2014-04-29 20:09:12 +0300

    Added a dummy app

---
+++ modules/user/controllers/user.js
@@ -0,0 +1,15 @@
+module.exports = {
+	doGet : function(){
+		return {action : 'to register users'};
+	},
+	doPost : function(){
+		return {action : 'to register users'};
+	},
+	doPut	: function(){
+		return {action : 'to upload files'};
+	},
+	doDelete: function(){
+		return {action : 'to delete'};
+	}
+
+};
